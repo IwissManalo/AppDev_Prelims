d5ecfdc59c8686bb96257d46bbeb4d2eb5b1c2a8
IwissManalo <altheairishmanalo@gmail.com>
2025-02-05 19:10:46 +0800

    Update tailwind.config.ts

--- thick_thread/tailwind.config.ts
+++ thick_thread/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'el-messiri': ['"El Messiri"', 'Arial', 'Helvetica', 'sans-serif'],
+      },
     },
   },
   plugins: [],
