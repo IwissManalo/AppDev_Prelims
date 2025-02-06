501b77be173bb58a7498edbe65ef360557d5bf19
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 16:34:42 +0800

    Create homepage.tsx

---
+++ thick_thread/components/landing/homepage.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Homepage() {
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 items-center sm:items-start">
+     
+      </main>
+      <footer className="flex gap-6 flex-wrap items-center justify-center">
+    
+      </footer>
+    </div>
+  );
+}
