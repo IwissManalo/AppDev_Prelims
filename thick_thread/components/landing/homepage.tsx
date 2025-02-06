97b62aa1fa5ec7934c9340959c56223dcf1e5734
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 17:09:57 +0800

    Update homepage.tsx

--- thick_thread/components/landing/homepage.tsx
+++ thick_thread/components/landing/homepage.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Homepage() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 items-center sm:items-start">
-     
+    <div className="min-h-screen pt-0 pb-20 gap-16 sm:pt-0 flex items-center justify-between">
+      <main className="flex flex-col gap-8 items-start sm:items-start">
+        <h1 className="text-4xl font-bold ">Welcome to Thick Thread</h1>
+        <p className="text-lg">Your one-stop solution for all threading needs. We provide high-quality threads for various applications.</p>
       </main>
-      <footer className="flex gap-6 flex-wrap items-center justify-center">
-    
-      </footer>
+      <div className="flex items-center">
+        <Image src="/TTlogo.png" alt="TT Logo" width={400} height={300} />
+      </div>
     </div>
   );
 }
