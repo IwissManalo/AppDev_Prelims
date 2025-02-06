b7667bfcd5e32536d52c713f670fe62b9929db3c
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 17:09:52 +0800

    Update layout.tsx

--- thick_thread/app/layout.tsx
+++ thick_thread/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { El_Messiri, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const elMessiri = El_Messiri({
   variable: "--font-el-messiri",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="font-el-messiri">
       <NavBar />
         {children}
+      <Footer />
       </body>
     </html>
   );
