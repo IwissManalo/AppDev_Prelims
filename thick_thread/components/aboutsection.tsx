78bb625d20ab798d680b9e3e73bfc2a0704a723a
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 20:19:12 +0800

    Create aboutsection.tsx

---
+++ thick_thread/components/aboutsection.tsx
@@ -0,0 +1,9 @@
+export default function AboutSection() {
+    return (
+        <section className="text-gray-600 body-font bg-white-100" id='contactUs'>
+            <p>
+                ADD ABOUT US TEASER HERE
+            </p>
+        </section>
+    );
+}
