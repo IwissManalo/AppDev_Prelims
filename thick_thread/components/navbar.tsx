c3bd84553291466e868510c641196bb4ea3d2fda
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 16:34:44 +0800

    Create navbar.tsx

---
+++ thick_thread/components/navbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+      <header className="flex justify-between items-center w-full p-4"  style={{ backgroundColor: '#F7C566' }}>
+        <div className="flex items-center gap-1">
+          <Image
+            src="/TTLogo.png"
+            alt="Brand Logo"
+            width={50}
+            height={50}
+          />
+          <span className="text-xl font-bold">Thick Thread</span>
+        </div>
+        <nav className="flex gap-4">
+          <a href="/" className="hover:underline">Home</a>
+          <a href="/shop" className="hover:underline">Shop</a>
+          <a href="/about" className="hover:underline">About Us</a>
+          <a href="/gallery" className="hover:underline">Gallery</a>
+          <a href="/blog" className="hover:underline">Blog</a>
+        </nav>
+      </header>
+  );
+}
