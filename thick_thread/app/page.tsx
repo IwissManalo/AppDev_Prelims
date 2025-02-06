856555216dd3933d85bd723b6e1cd50cc3d40049
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 20:19:07 +0800

    Update page.tsx

--- thick_thread/app/page.tsx
+++ thick_thread/app/page.tsx
@@ -1,11 +1,13 @@
+import AboutSection from "@/components/aboutsection";
+import ContactCard from "@/components/contactcard";
 import Homepage from "@/components/landing/homepage";
 
 export default function Home() {
   return (
     <div>
       <Homepage />
-      {/* <Cards />
-      <ContactUs /> */}
+      <AboutSection />
+      <ContactCard />
     </div>
   );
 }
