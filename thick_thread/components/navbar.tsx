3757a35f97ab82a4846fd96fa5590d3bc20b9937
IwissManalo <altheairishmanalo@gmail.com>
2025-02-06 17:09:59 +0800

    Update navbar.tsx

--- thick_thread/components/navbar.tsx
+++ thick_thread/components/navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-      <header className="flex justify-between items-center w-full p-4"  style={{ backgroundColor: '#F7C566' }}>
+      <header className="flex justify-between items-center w-full p-4 shadow-md sticky top-0"  style={{ backgroundColor: '#F7C566' }}>
         <div className="flex items-center gap-1">
           <Image
             src="/TTLogo.png"
@@ -10,9 +10,9 @@ export default function Navbar() {
             width={50}
             height={50}
           />
-          <span className="text-xl font-bold">Thick Thread</span>
+          <span className="text-xl font-bold pt-3">Thick Thread</span>
         </div>
-        <nav className="flex gap-4">
+        <nav className="flex gap-4 pt-2">
           <a href="/" className="hover:underline">Home</a>
           <a href="/shop" className="hover:underline">Shop</a>
           <a href="/about" className="hover:underline">About Us</a>
