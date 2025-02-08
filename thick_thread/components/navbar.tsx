8d710a4b4d8830b9d983ac58993dca0eb2c7b685
IwissManalo <altheairishmanalo@gmail.com>
2025-02-08 10:29:03 +0800

    Update navbar.tsx

--- thick_thread/components/navbar.tsx
+++ thick_thread/components/navbar.tsx
@@ -1,24 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-      <header className="flex justify-between items-center w-full p-4 shadow-md sticky top-0"  style={{ backgroundColor: '#F7C566' }}>
-        <div className="flex items-center gap-1">
+    <header className="flex justify-between items-center w-full p-4 shadow-md sticky top-0" style={{ backgroundColor: '#F7C566' }}>
+      <div className="flex items-center gap-1">
+        <Link href="/">
           <Image
             src="/TTLogo.png"
             alt="Brand Logo"
             width={50}
             height={50}
+            className="cursor-pointer"
           />
-          <span className="text-xl font-bold pt-3">Thick Thread</span>
-        </div>
-        <nav className="flex gap-4 pt-2">
-          <a href="/" className="hover:underline">Home</a>
-          <a href="/shop" className="hover:underline">Shop</a>
-          <a href="/about" className="hover:underline">About Us</a>
-          <a href="/gallery" className="hover:underline">Gallery</a>
-          <a href="/blog" className="hover:underline">Blog</a>
-        </nav>
-      </header>
+        </Link>
+        <span className="text-xl font-bold pt-3">Thick Thread</span>
+      </div>
+      <nav className="flex gap-4 pt-2">
+        <Link href="/" className="hover:underline">Home</Link>
+        <Link href="/shop" className="hover:underline">Shop</Link>
+        <Link href="/about" className="hover:underline">About Us</Link>
+        <Link href="/gallery" className="hover:underline">Gallery</Link>
+        <Link href="/blog" className="hover:underline">Blog</Link>
+      </nav>
+    </header>
   );
 }
