84e5f5aeeafed4e4b7974255d2b5b2d5cdfda9ad
IwissManalo <altheairishmanalo@gmail.com>
2025-02-08 08:50:27 +0800

    Update shopsection.tsx

--- thick_thread/components/landing/shopsection.tsx
+++ thick_thread/components/landing/shopsection.tsx
@@ -2,16 +2,16 @@ import ShopCard from "../shopcard";
 
 export default function ShopSection() {
   return (
-    <div className="bg-[#DC6B19] py-10"> 
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 px-6">
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
-        <ShopCard image="/TTLogo.png" />
+    <div className="bg-[#DC6B19] py-10">
+      <div className="container mx-auto px-6 text-center">
+        <h1 className="text-3xl font-bold text-white underline">Shop</h1>
+        <p className="text-white mb-6">where every stitch transforms imagination into art.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          <ShopCard image="/TTLogo.png" />
+          <ShopCard image="/TTLogo.png" />
+          <ShopCard image="/TTLogo.png" />
+          <ShopCard image="/TTLogo.png" />
+        </div>
       </div>
     </div>
   );
