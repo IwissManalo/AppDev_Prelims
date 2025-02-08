0dd6ea32b16fb4a3cb71b0b2592c52ade6782e81
IwissManalo <altheairishmanalo@gmail.com>
2025-02-08 08:39:13 +0800

    Update page.tsx

--- thick_thread/app/about/page.tsx
+++ thick_thread/app/about/page.tsx
@@ -1,5 +1,7 @@
+import ContactCard from "@/components/contactcard";
+
 export default function About() {
     return (
-       <h1>This is the About Page</h1>
+       <><h1>This is the About Page</h1><ContactCard /></>
     );
 }
