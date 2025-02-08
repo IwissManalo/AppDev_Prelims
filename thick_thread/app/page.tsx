39ad4b8309ffb1d8ebf09b8c621ff8fddf6e85ed
IwissManalo <altheairishmanalo@gmail.com>
2025-02-08 10:02:45 +0800

    Update page.tsx

--- thick_thread/app/page.tsx
+++ thick_thread/app/page.tsx
@@ -1,12 +1,14 @@
 import ShopSection from "@/components/landing/shopsection";
 import ContactCard from "@/components/contactcard";
 import { AboutSection } from "@/components/landing/aboutsection";
+import GallerySection from "@/components/landing/gallerysection";
 
 export default function Home() {
   return (
     <div>
       <AboutSection />
       <ShopSection />
+      <GallerySection />
       <ContactCard />
     </div>
   );
