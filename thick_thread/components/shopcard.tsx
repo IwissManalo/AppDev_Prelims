0beaf78e4963520132bc4fa5666447adb8a7a61f
IwissManalo <altheairishmanalo@gmail.com>
2025-02-08 08:50:29 +0800

    Update shopcard.tsx

--- thick_thread/components/shopcard.tsx
+++ thick_thread/components/shopcard.tsx
@@ -7,12 +7,12 @@ interface ShopCardProps {
 
 export default function ShopCard({ image, buttonText = "Order Now" }: ShopCardProps) {
   return (
-    <div className="bg-[#FFF8DC] p-4 rounded-lg shadow-lg flex flex-col items-center">
-      <div className="w-40 h-56 bg-cream border rounded-lg flex items-center justify-center">
+    <div className="bg-[#FFF8DC] p-6 rounded-lg shadow-lg flex flex-col items-center w-70 h-80">
+      <div className="w-48 h-64 bg-cream border rounded-lg flex items-center justify-center">
         {image ? <img src={image} alt="Product" className="w-full h-full object-cover rounded-lg" /> : "Image here"}
       </div>
       <button
-        className="mt-4 px-4 py-2 text-white rounded cursor-pointer"
+        className="mt-4 px-12 py-2 text-white rounded cursor-pointer"
         style={{ backgroundColor: '#DC6B19', transition: 'background-color 0.3s ease, color 0.3s ease' }}
         onMouseEnter={(e) => {
           e.currentTarget.style.backgroundColor = '#F7C566';
